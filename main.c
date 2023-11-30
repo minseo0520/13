e1b6be78ddf33287c9fcd97ea7781dba1316e4b8
minseo0520 <141472390+minseo0520@users.noreply.github.com>
2023-11-30 15:30:27 +0900

    실습1-2

--- main.c
+++ main.c
@@ -10,7 +10,20 @@ struct student{
 };
 
 int main(int argc, char *argv[]) {
+	//instance 선언 
 	struct student s1 = {123, "MinSeo", 4.3};
+	
+	//다른 값 변경 
+	s1.ID = 123456;
+	s1.name[0] = 'c';
+	s1.score = 0.7;
+	 
+	printf("ID : %d\n", s1.ID);
+	printf("name : %s\n", s1.name);
+	printf("score : %f\n", s1.score);
+	
+	strcpy(s1.name, "Cheawon"); 
+	printf("name2 : %s\n", s1.name);
 	
 	return 0;
 }
